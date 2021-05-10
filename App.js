d5390090a35bf961b68639a5bfba8fc428835c1f
debe0025 <jules.debeaumont@etudiant.univ-reims.fr>
2021-05-10 10:25:12 +0200

    change calculatrice into app with title and status bar

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Calculatrice } from './components/Calculatrice';
-import { Title } from './components/Title';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.bar}>
-        <StatusBar style="light" />
-        <Title title="Ma Calculatrice" />
-      </View>
-      <View>
-        <Calculatrice />
-      </View>
+      <Calculatrice />
     </View>
   );
 }
@@ -22,8 +14,5 @@ const styles = StyleSheet.create({
   container: {
     color: '#FFFFFF',
     flex: 1,
-  },
-  bar: {
-    backgroundColor: '#707070',
   },
 });
--- components/Calculatrice.js
+++ components/Calculatrice.js
@@ -1,17 +1,29 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { Expression } from './Expression';
 import { Keyboard } from './Keyboard';
 import { Memory } from './Memory';
+import { Title } from './Title';
 
 export function Calculatrice() {
   return (
     <View>
+      <View style={styles.bar}>
+        <StatusBar style="light" />
+        <Title title="Ma Calculatrice" />
+      </View>
       <Expression expr="expression" />
       <Memory />
       <Keyboard />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  bar: {
+    backgroundColor: '#707070',
+  },
+});
 
 export default Calculatrice;
